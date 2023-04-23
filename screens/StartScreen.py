3ceecc7585e7321cea68d6b6e6bae51d8c28b8e9
Richard Hofmänner <hello@terillos.dev>
2023-04-23 14:48:18 +0200

    Created Main Menu

--- screens/StartScreen.py
+++ screens/StartScreen.py
@@ -1,8 +1,30 @@
 import tkinter as tk
-class StartScreen(tk.Frame):
+from tkinter import ttk
+import config.styleings as style
 
-  def __init__(self, parent):
-    super().__init__(parent)
-    tk.Label(self, text = "This is Window 1").pack(padx = 10, pady = 10)
-    tk.Button(self, text="Hello World").pack()
-    self.pack(padx = 10, pady = 10)
+
+class StartScreen():
+    def __init__(self):
+        window = tk.Tk()
+        window.title("Main Menu")
+        window.geometry("600x400")
+
+        for x in range(5):
+            window.rowconfigure(x, weight=1)
+
+        for y in range(3):
+            window.columnconfigure(y, weight=1)
+
+
+        titleLabel = tk.Label(window, text="Memory: THE GAME", **style.h1Style)
+        titleLabel.grid(row=1, column=1, sticky="nsew")
+
+        startButton = tk.Button(window, text="Start", **style.buttonStyle, command=lambda: print("Start"))
+        startButton.grid(row=2, column=1)
+
+        quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
+        quitButton.grid(row=4, column=1)
+        
+        window.minsize(600, 400)
+        window.maxsize(600, 400)
+        window.mainloop()

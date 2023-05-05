0a2808dde7914557b0d5f2a8c86f558f05f44030
Nikola Miletic <35v3721@bztf.ch>
2023-05-05 13:28:47 +0200

    commit

--- screens/StartScreen.py
+++ screens/StartScreen.py
@@ -7,7 +7,8 @@ class StartScreen():
     def __init__(self):
         window = tk.Tk()
         window.title("Main Menu")
-        window.geometry("600x400")
+        window.geometry("600x300")
+        window.configure(background=style.mainBgColor)
 
         for x in range(5):
             window.rowconfigure(x, weight=1)
@@ -18,13 +19,26 @@ class StartScreen():
 
         titleLabel = tk.Label(window, text="Memory: THE GAME", **style.h1Style)
         titleLabel.grid(row=1, column=1, sticky="nsew")
-
+        
         startButton = tk.Button(window, text="Start", **style.buttonStyle, command=lambda: print("Start"))
         startButton.grid(row=2, column=1)
-
+        
+        helpbutton = tk.Button(window, text="Help", **style.buttonStyle, command= HelpScreen.HelpWindow)
+        helpbutton.grid(row=3, column=1)
+        
         quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
         quitButton.grid(row=4, column=1)
         
         window.minsize(600, 400)
         window.maxsize(600, 400)
         window.mainloop()
+        
+class HelpScreen():
+    def HelpWindow():
+        window2 = tk.Tk()
+        window2.title("Help Menu")
+        window2.geometry("300x200")
+        window2.configure(background=style.mainBgColor)
+        
+        Guidelabel = tk.Label(window2, text="Guide:")
+        Guidelabel.grid(row=2, column=1)

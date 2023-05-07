dd5225c6f901f8ae1e1173295962275b5c951cdc
mathias.bächler <35s6246@bztf.ch>
2023-05-07 18:05:00 +0200

    gamescreen_change_07.05_18:05

--- screens/Gamescreen.py
+++ screens/Gamescreen.py
@@ -1,19 +1,31 @@
+from screens.StartScreen import GameScreen
 import tkinter as tk
-class GameScreen(tk.Frame):
+from tkinter import ttk
+import config.styleings as style
 
-  def __init__(self, parent):
-    super().__init__(parent)
-    tk.Label(self, text = "This is the Gamescreen").pack(padx = 10, pady = 10)
-    
-# Erstellung der Karten (hier als Labels)
-symbols = ["A", "B", "C", "D", "E", "F", "G", "H"]
-cards = symbols * 2  # 8 Paare
-card_labels = []
-for i in range(4):
-    for j in range(4):
-        card = tk.Label(game_frame, text="", font=("Arial", 24), width=3, height=2, relief="groove")
-        card.grid(row=i, column=j, padx=5, pady=5)
-        card_labels.append(card)
 
-# Start des GUI-Loops
-root.mainloop()
+class Gamescreen():
+    def Gamescreen(self): ## 
+        window = tk.Tk()
+        window.title("Game Screen")
+        window.geometry("600x400")
+        window.configure(background=style.mainBgColor)
+
+        for x in range(4):
+            window.rowconfigure(x, weight=1)
+
+        for y in range(6):
+            window.columnconfigure(y, weight=1)
+
+        startButton = tk.Button(window, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"))
+        startButton.grid(row=1, column=1)
+        
+        quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
+        quitButton.grid(row=4, column=6)
+        
+        Gamescreen.mainloop() 
+        
+    def Createcards(self): ##
+        window = tk.Tk()    
+            
+        window.mainloop() 

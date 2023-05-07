7b27513adda9f36ca7baa23ca5caba56fa4b0e49
mathias.bächler <35s6246@bztf.ch>
2023-05-07 20:51:39 +0200

    test_07.05_20:53

--- screens/Gamescreen.py
+++ screens/Gamescreen.py
@@ -11,21 +11,22 @@ class Gamescreen():
         window.geometry("600x400")
         window.configure(background=style.mainBgColor)
 
-        for x in range(4):
+        for x in range(6):
             window.rowconfigure(x, weight=1)
 
         for y in range(6):
             window.columnconfigure(y, weight=1)
 
-        startButton = tk.Button(window, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"))
-        startButton.grid(row=1, column=1)
-        
-        button = tk.Button( width=10, height=5)
-        
-        
+        startButton = tk.Button(window, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
+        startButton.grid(row=0, column=1)
         
         quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
-        quitButton.grid(row=4, column=6)
+        quitButton.grid(row=6, column=1)
+        
+        for i in range(4):
+            for j in range(4):
+                cardButton = tk.Button(window, text=f"Button {(i*4)+j+1}", **style.buttonStyle, command=lambda: print("Button {(i*4)+j+1}"), width=10, height=5)
+                cardButton.grid(row=1+i, column=1+j)
         
         Gamescreen.mainloop() 
         

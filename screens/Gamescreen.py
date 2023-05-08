4ec9ed78f7bf86f9c968ce1cf84e2f3bc9bf4d5a
mathias.bächler <35s6246@bztf.ch>
2023-05-08 21:56:05 +0200

    test_08.05_21:55_cardbutton

--- screens/Gamescreen.py
+++ screens/Gamescreen.py
@@ -6,30 +6,32 @@ import config.styleings as style
 
 class Gamescreen():
     def Gamescreen(self): ## 
-        window = tk.Tk()
-        window.title("Game Screen")
-        window.geometry("600x400")
-        window.configure(background=style.mainBgColor)
+        window3 = tk.Tk()
+        window3.title("Game Screen")
+        window3.geometry("600x400")
+        window3.configure(background=style.mainBgColor)
 
         for x in range(6):
-            window.rowconfigure(x, weight=1)
+            window3.rowconfigure(x, weight=1)
 
         for y in range(6):
-            window.columnconfigure(y, weight=1)
+            window3.columnconfigure(y, weight=1)
 
-        startButton = tk.Button(window, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
-        startButton.grid(row=0, column=0)
-        
-        quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
-        quitButton.grid(row=6, column=6)
-        
         cardButtons = []
         for i in range(4):
             row = i // 4 + 1
             column = i % 4 + 1
-            cardButton = tk.Button(self.window, text=f"Card {i+1}", width=10, height=5)
+            cardButton = tk.Button(self.window3, text=f"Card {i+1}", width=10, height=5)
             cardButton.grid(row=row, column=column)
             cardButtons.append(cardButton)
+
+        startButton = tk.Button(window3, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
+        startButton.grid(row=0, column=0)
+        
+        quitButton = tk.Button(window3, text="Quit", **style.buttonStyle, command=lambda: window3.destroy())
+        quitButton.grid(row=6, column=6)
+        
+
         
         Gamescreen.mainloop() 
         

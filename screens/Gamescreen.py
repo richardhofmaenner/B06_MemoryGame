5c5088c36196ac0b491a177c876a235dafa5235a
Richard Hofmänner <hello@terillos.dev>
2023-05-12 11:24:43 +0200

    Fixed Counter Display Issue

--- screens/Gamescreen.py
+++ screens/Gamescreen.py
@@ -1,51 +1,67 @@
-from screens.StartScreen import GameScreen
 import tkinter as tk
+
 from tkinter import ttk
+
 import config.styleings as style
 
 
+
 class Gamescreen():
-    def Gamescreen(self): ## 
-        window3 = tk.Tk()
-        window3.title("Game Screen")
-        window3.geometry("600x400")
-        window3.configure(background=style.mainBgColor)
+
+    clicks = 0
+    gameWindow = None
+    countButton = None
+
+    def __init__(self): ## 
+        self.gameWindow = tk.Tk()
+        self.gameWindow.title("Game Screen")
+        self.gameWindow.geometry("600x400")
+        self.gameWindow.configure(background=style.mainBgColor)
+
 
         for x in range(6):
-            window3.rowconfigure(x, weight=1)
+            self.gameWindow.rowconfigure(x, weight=1)
+
 
         for y in range(6):
-            window3.columnconfigure(y, weight=1)
+           self.gameWindow.columnconfigure(y, weight=1)
+
 
         cardButtons = []
+
         for i in range(16):
+
             row = i // 4 + 1
+
             column = i % 4 + 1
-            cardButton = tk.Button(self.window3, text=f"Card {i+1}", width=10, height=5)
+
+            cardButton = tk.Button(self.gameWindow, text=f"Card {i+1}", width=10, height=5, command=self.count_clicks)
             cardButton.grid(row=row, column=column)
+
             cardButtons.append(cardButton)
 
-        startButton = tk.Button(window3, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
+
+        startButton = tk.Button(self.gameWindow, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
+
         startButton.grid(row=0, column=0)
         
-        quitButton = tk.Button(window3, text="Quit", **style.buttonStyle, command=lambda: window3.destroy())
+
+        quitButton = tk.Button(self.gameWindow, text="Quit", **style.buttonStyle, command=lambda: self.gameWindow.destroy())
+
         quitButton.grid(row=6, column=6)
+
+        self.countButton = tk.Button(self.gameWindow, text=f"Click Counts: {self.clicks}", width=10, height=3 )
+        self.countButton.grid(row=0, column=5)
+
+        gameWindow.mainloop()
         
-    clicks = 0
+
 
     def count_clicks(self):
-        global clicks
-        clicks += 1
-        
-        
-        countButton = tk.Button(self.window3, print(f"Total clicks: {clicks}"), width=10, height=3 )
-        countButton.grid(row=0, column=6)
-        
-        Gamescreen.mainloop() 
-        
-    ##def Createcards(self): ## ev. separate def?
-    ##    window = tk.Tk()    
-        
-        
-            
-    ##    window.mainloop() 
+        self.clicks += 1
+        self.countButton.config(text=f"Total clicks: {self.clicks}")
+        print(f"Total clicks: {self.clicks}")
+
+
+    def get_clicks_text(self):
+        return f"Total Clicks: {self.clicks}"

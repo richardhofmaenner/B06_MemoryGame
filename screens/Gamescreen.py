bef57b5cc940259a64076d47476530f3276d7c54
mathias bächler <35s6246@bztf.ch>
2023-06-02 11:11:00 +0200

    text entfernt

--- screens/Gamescreen.py
+++ screens/Gamescreen.py
@@ -1,10 +1,6 @@
 import tkinter as tk
-
 from tkinter import ttk
-
 import config.styleings as style
-
-
 
 class Gamescreen():
 
@@ -18,44 +14,33 @@ class Gamescreen():
         self.gameWindow.geometry("600x400")
         self.gameWindow.configure(background=style.mainBgColor)
 
-
         for x in range(6):
             self.gameWindow.rowconfigure(x, weight=1)
-
 
         for y in range(6):
            self.gameWindow.columnconfigure(y, weight=1)
 
-
         cardButtons = []
 
         for i in range(16):
-
             row = i // 4 + 1
-
             column = i % 4 + 1
 
             cardButton = tk.Button(self.gameWindow, text=f"Card {i+1}", width=10, height=5, command=self.count_clicks)
             cardButton.grid(row=row, column=column)
-
             cardButtons.append(cardButton)
 
-
         startButton = tk.Button(self.gameWindow, text="Start Game", **style.buttonStyle, command=lambda: print("Start Game"), width=10, height=3)
-
         startButton.grid(row=0, column=0)
         
-
         quitButton = tk.Button(self.gameWindow, text="Quit", **style.buttonStyle, command=lambda: self.gameWindow.destroy())
-
         quitButton.grid(row=6, column=6)
 
         self.countButton = tk.Button(self.gameWindow, text=f"Click Counts: {self.clicks}", width=10, height=3 )
         self.countButton.grid(row=0, column=5)
 
-        gameWindow.mainloop()
+        self.gameWindow.mainloop()
         
-
 
     def count_clicks(self):
         self.clicks += 1

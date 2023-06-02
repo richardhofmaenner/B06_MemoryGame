90e8491238c1efee4c3794ae391358829351d840
Richard Hofmänner <hello@terillos.dev>
2023-06-02 18:53:50 +0200

    Cleaned up some code

--- screens/StartScreen.py
+++ screens/StartScreen.py
@@ -1,50 +1,52 @@
 import tkinter as tk
 from tkinter import ttk
 import config.stylings as style
-from screens.Gamescreen import Gamescreen
+from screens.GameScreen import GameScreen
+from screens.HelpScreen import HelpScreen
 
 
 class StartScreen():
+
+    startScreenWindow = None
+
     def __init__(self):
-        window = tk.Tk()
-        window.title("Main Menu")
-        window.geometry("600x300")
-        window.configure(background=style.mainBgColor)
+        self.startScreenWindow = tk.Tk()
+        self.startScreenWindow.title("Main Menu")
+        self.startScreenWindow.geometry("600x400")
+        self.startScreenWindow.configure(background=style.mainBgColor)
 
         for x in range(5):
-            window.rowconfigure(x, weight=1)
+            self.startScreenWindow.rowconfigure(x, weight=1)
 
         for y in range(3):
-            window.columnconfigure(y, weight=1)
+            self.startScreenWindow.columnconfigure(y, weight=1)
 
-
-        titleLabel = tk.Label(window, text="Memory: THE GAME", **style.h1Style)
+        titleLabel = tk.Label(self.startScreenWindow,
+                              text="Memory: THE GAME", **style.h1Style)
         titleLabel.grid(row=1, column=1, sticky="nsew")
-        
-        startButton = tk.Button(window, text="Single Player", **style.buttonStyle, command=self.Gamescreen)
+
+        startButton = tk.Button(self.startScreenWindow, text="Single Player",
+                                **style.buttonStyle, command=self.openGameScreen)
         startButton.grid(row=2, column=1)
-        
-        helpbutton = tk.Button(window, text="Help", **style.buttonStyle, command= HelpScreen.HelpWindow)
+
+        helpbutton = tk.Button(self.startScreenWindow, text="Help",
+                               **style.buttonStyle, command=self.openHelpScreen)
         helpbutton.grid(row=4, column=1)
-        
-        quitButton = tk.Button(window, text="Quit", **style.buttonStyle, command=lambda: window.destroy())
-        quitButton.grid(row=5, column=1)
-        
-        window.minsize(600, 400)
-        window.maxsize(600, 400)
-        window.mainloop()
-    
 
-    def Gamescreen(self):
-        gameScreenWindow = Gamescreen()
+        quitButton = tk.Button(self.startScreenWindow, text="Quit", **
+                               style.buttonStyle, command=self.closeStartScreen)
+        quitButton.grid(row=5, column=1, pady=10, padx=10)
 
+        self.startScreenWindow.minsize(600, 400)
+        self.startScreenWindow.maxsize(600, 400)
+        self.startScreenWindow.mainloop()
 
-class HelpScreen():
-    def HelpWindow():
-        window2 = tk.Tk()
-        window2.title("Help Menu")
-        window2.geometry("300x200")
-        window2.configure(background=style.mainBgColor)
-        
-        Guidelabel = tk.Label(window2, text="Guide:")
-        Guidelabel.grid(row=2, column=1)
+    def openGameScreen(self):
+        self.startScreenWindow.destroy()
+        GameScreen()
+
+    def closeStartScreen(self):
+        self.startScreenWindow.destroy()
+
+    def openHelpScreen(self):
+        HelpScreen()
